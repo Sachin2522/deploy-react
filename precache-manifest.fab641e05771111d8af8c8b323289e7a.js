db0492d97b072b147948266852b133a613a6fc61
Web Dev <Web Dev@DESKTOP-6GJ3D01>
2019-11-23 11:45:46 +0530

    Updates

---
+++ precache-manifest.fab641e05771111d8af8c8b323289e7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17270177e9bd6b5a65d1bb0232276838",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "08cb4065948aedb9b461",
+    "url": "/deploy-react/static/css/main.5b1ef669.chunk.css"
+  },
+  {
+    "revision": "99a8a2c319e36d879a56",
+    "url": "/deploy-react/static/js/2.6daf7eec.chunk.js"
+  },
+  {
+    "revision": "08cb4065948aedb9b461",
+    "url": "/deploy-react/static/js/main.4b5e9395.chunk.js"
+  },
+  {
+    "revision": "35216c25b5c301439205",
+    "url": "/deploy-react/static/js/runtime-main.5f10b04a.js"
+  }
+]);
